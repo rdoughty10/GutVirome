146691678fcbb14a697d59033b5cff8a8dbb279b
Ryan Doughty <rdd57@case.edu>
2023-02-22 10:10:03 +0100

    added util file for base functions

---
+++ src/util/util.py
@@ -0,0 +1,8 @@
+"""Util file containing random useful functions used throughout"""
+import os
+
+
+def mkdir_p(directory:str):
+    '''make directory if does not exist'''
+    if not os.path.exists(directory):
+        os.mkdir(directory)
